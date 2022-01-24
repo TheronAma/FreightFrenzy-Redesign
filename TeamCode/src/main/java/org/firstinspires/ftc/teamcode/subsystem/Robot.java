5bc3735ad0a962492e5a7f58cd837763c866c172
TheronAma <theron945@gmail.com>
2022-01-24 12:28:02 -0500

    Create base Robot and Subsystem classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Robot.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.subsystem;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import java.util.ArrayList;
+
+public class Robot {
+    private Telemetry telemetry;
+    private HardwareMap hardwareMap;
+
+    private ArrayList<Subsystem> subsystems;
+
+    public Robot(Telemetry telemetry, HardwareMap hardwareMap){
+        this.telemetry = telemetry;
+        this.hardwareMap = hardwareMap;
+    }
+}
