883bc1d9ea120ce1dd361f4999ddadd35db74a2b
TheronAma <theron945@gmail.com>
2022-03-05 11:59:01 -0500

    Add Drivetrain and Lift subsystems to Robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Robot.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.subsystem;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.subsystem.drive.Drivetrain;
+import org.firstinspires.ftc.teamcode.subsystem.lift.Lift;
 
 import java.util.ArrayList;
 
@@ -10,11 +12,20 @@ public class Robot {
     private Telemetry telemetry;
     private HardwareMap hardwareMap;
 
+    public Lift lift;
+    public Drivetrain drive;
+
     private ArrayList<Subsystem> subsystems;
 
     public Robot(Telemetry telemetry, HardwareMap hardwareMap){
         this.telemetry = telemetry;
         this.hardwareMap = hardwareMap;
+
+        subsystems = new ArrayList<>();
+        subsystems.add((Subsystem) drive);
+        subsystems.add((Subsystem) lift);
+//        subsystems.add((Subsystem) drive);
+//        subsystems.add((Subsystem) drive);
     }
 
     public void update() {
